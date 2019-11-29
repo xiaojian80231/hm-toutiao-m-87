43bd4a60c7792f37504cd75778ca1358f15badb6
xiaojian80231 <54383677+xiaojian80231@users.noreply.github.com>
2019-11-29 17:02:52 +0800

    初始化项目

---
+++ src/views/Layout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <!-- 导航栏 -->
+    <!-- 内容容器 -->
+    <!-- tab栏 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: ''
+}
+</script>
+
+<style lang="less" scoped>
+</style>
